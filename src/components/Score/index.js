b934fd9756fdabf53db757956d9340290c74ba01
Natalie Brickley <natt_rose@yahoo.com>
2019-04-23 09:39:46 -0700

    changed title

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -4,7 +4,7 @@ function Score(props) {
     return (
         <div>
             <nav className="navbar fixed-top">
-                <div className="navbar-brand mb-0 h1 title">Clicky Kitties</div>
+                <div className="navbar-brand mb-0 h1 title">Clickity Cat</div>
                 <div className={props.navclass}>{props.navtext}</div>
                 <div className="score">Score: {props.score} | Top Score: {props.topscore}</div>
             </nav>
